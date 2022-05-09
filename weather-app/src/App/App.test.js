ce24fb255443b897454fd213dd05f43c69e03aeb
Wesley Kelly <wesley.kelly@hotmail.com>
2022-05-09 15:26:42 +0100

    phone

--- weather-app/src/App/App.test.js
+++ weather-app/src/App/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Title from "../App/components/Title"
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+
